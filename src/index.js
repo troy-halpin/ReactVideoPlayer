e2a78e6b5713867d2ea02469b89a614aac61d610
troy-halpin <troyhalpin1@gmail.com>
2017-05-10 15:20:28 -0400

    adding existing work to github

--- src/index.js
+++ src/index.js
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar.js';
+import VideoList from './components/video_list.js';
+import VideoDetail from './components/video_detail.js';
+const API_KEY = 'reobtain from online';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+//create a component
+// const App = () => {
+// 	return (
+// 		<div>
+// 			<SearchBar />
+// 		</div>
+// 	);
+// };
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { 
+			videos: [],
+			selectedVideo: null
+		};
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ 
+				videos: videos,
+				selectedVideo: videos[0]
+			});
+		});
+	};
+
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
+}
+
+//place in DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
+
+
