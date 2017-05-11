46b8c422ff320de47305d558b317f28993af7db2
Troy Halpin <troyhalpin1@gmail.com>
2017-05-10 23:58:04 -0400

    finished video player, search works and search while typing is semi throttled. a bit of styling thrown in too

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar.js';
 import VideoList from './components/video_list.js';
 import VideoDetail from './components/video_detail.js';
-const API_KEY = 'reobtain from online';
+const API_KEY = '';
 
 
 //create a component
@@ -25,21 +26,29 @@ class App extends Component {
 			selectedVideo: null
 		};
 
-		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+		this.videoSearch('surfboards');
+	};
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
 			this.setState({ 
 				videos: videos,
 				selectedVideo: videos[0]
 			});
 		});
-	};
-
+	}
 
 	render() {
+
+		const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
 		return (
 			<div>
-				<SearchBar />
+				<SearchBar onSearchTermChange={videoSearch} />
 				<VideoDetail video={this.state.selectedVideo} />
-				<VideoList videos={this.state.videos} />
+				<VideoList 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+					videos={this.state.videos} />
 			</div>
 		);
 	}
